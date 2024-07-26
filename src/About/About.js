cad8f75831b48a2dce81be3b3b936e9b362edbe6
Racheline <136965489+Rachel2001@users.noreply.github.com>
2024-07-26 13:35:32 +0200

    more debugging

--- src/About/About.js
+++ src/About/About.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import personalPic from "../images/personalpic.png";
 import AOS from "aos";
